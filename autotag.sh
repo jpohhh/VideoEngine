94f12e2b415feb3343cbe21de733663521b9b26d
refulgentis <refulgentis@a0050936-f7bc-11dd-a320-db7d8e7a638f>
2009-02-13 07:44:27 +0000

    expect AtomicParsley64 in AppSupport, cleanup  all curl'd data from thetvdb after tagging

--- autotag.sh
+++ autotag.sh
@@ -72,8 +72,11 @@ do
 		curl http://images.thetvdb.com/banners/$cover_art_url > $HOME/Library/Application\ Support/Engine/coverart.jpg 2>>/dev/null
 
 		echo "7) PULLED TAGS Show name:"$cart "Episode name:"$cnam "Aired:"$cday "Description:"$desc "Stik:"$stik "tven:"$tven "tvnn:"$tvnn "cover_art_url:"$cover_art_url >> "$logfile"
-		atomicparsley64 "$1" --title "$cnam" --artist "$cart" --year $cday --longDesc "$desc" --desc "$desc" --stik value=$stik --TVShowName "$cart" --TVEpisode $tven --TVNetwork $tvnn  --TVSeasonNum $season --TVEpisodeNum $episode --artwork $HOME/Library/Application\ Support/Engine/coverart.jpg --overWrite
-
+		$HOME/Library/Application\ Support/Engine/atomicparsley64 "$1" --title "$cnam" --artist "$cart" --year $cday --longDesc "$desc" --desc "$desc" --stik value=$stik --TVShowName "$cart" --TVEpisode $tven --TVNetwork $tvnn  --TVSeasonNum $season --TVEpisodeNum $episode --artwork $HOME/Library/Application\ Support/Engine/coverart.jpg --overWrite
+		
+		rm "$series_data"
+		rm $HOME/Library/Application\ Support/Engine/coverart.jpg
+		rm $HOME/Library/Application\ Support/Engine/episode.xml
 	#check if file is HD: if so, tag it with mp4tags
 		res=$(mp4track --list "$1" | grep -m 1 width | awk '{print $3}' | awk -F. '{print $1}')
 		if (($res>1270))
